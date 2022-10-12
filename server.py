7ba0c243d9174f5324946bc4c232b5db73e9fa47
Brian <bfeddes@hotmail.com>
2022-10-11 23:03:40 -0500

    Fully functional. Need to handle disconnect output

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ class ConnectionClosed(Exception):
     pass
 
 
-def receive_line(conn): # receive a single line that ends with a period
+def receive_line(conn):
     chunks = []
     while True:
         data = conn.recv(2000)
@@ -16,27 +16,32 @@ def receive_line(conn): # receive a single line that ends with a period
         chunks.append(data.decode('utf-8'))
         if ''.join(chunks).endswith('.'):
             break
-
     return ''.join(chunks)
 
 
 def handle_client(conn):
-    while True: 
+    thread_count = 1
+    while True:
         try:
             line = receive_line(conn)
             conn.sendall('Echo: {}'.format(line).encode('utf-8'))
         except ConnectionClosed:
+            print('<Thread-{}> closed connection.'.format(thread_count))
             break
-        print('closed connection')
 
 
 def listen():
+    print('<{}> thread handling main loop'.format(threading.current_thread().getName()))
+    thread_count = 1
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     s.bind(('0.0.0.0', 8080))
     s.listen(20)
     while True:
         conn, remote_addr = s.accept()
-        print('[+] connection from {}'.format(remote_addr))
+        print('<Thread-{}> - [+] connection from {}, spinning a new thread to handle it.'.format
+              (thread_count, remote_addr))
         thread = threading.Thread(target=handle_client, args=[conn], daemon=True)
+        thread_count += 1
         thread.start()
 
